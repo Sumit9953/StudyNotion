7e498393dc0b82487b432b4c5d3389f33d60bec9
sumit <sumitsantosh555@gmail.com>
2023-08-24 19:00:56 +0530

    url

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://study-notion60.vercel.app/",
     credentials: true,
   })
 );
